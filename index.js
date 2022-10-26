c73cfa5dcfb2c48853fcb0248e663e726ea72f12
sneoo <epicshakil@gmail.com>
2022-10-27 00:15:45 +0600

    updated data

--- index.js
+++ index.js
@@ -16,6 +16,18 @@ app.get("/courses", (req, res) => {
   res.send(courses);
 });
 
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const course = courses.find((n) => n.id === id);
+  res.send(course);
+});
+
+app.get("/checkout/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const course = courses.find((n) => n.id === id);
+  res.send(course);
+});
+
 app.listen(port, () => {
   console.log(`e-learning server is running on port: ${port}`);
 });
